4eda245380eb21d7d214ff7994575e1ee41e2395
akhileshsingla <h20180260@pilani.bits-pilani.ac.in>
2020-05-08 17:16:22 +0530

    Updates

---
+++ precache-manifest.03c393775d8115be3bad6637ebfe22fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ad8071e89a327ec2e79f78865da4deb",
+    "url": "/MyFirstReactWebApp/index.html"
+  },
+  {
+    "revision": "8f7f236f7be65c6cccb3",
+    "url": "/MyFirstReactWebApp/static/css/main.6a6b9995.chunk.css"
+  },
+  {
+    "revision": "dde33db50032b86889a4",
+    "url": "/MyFirstReactWebApp/static/js/2.7ed8d7e2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MyFirstReactWebApp/static/js/2.7ed8d7e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f7f236f7be65c6cccb3",
+    "url": "/MyFirstReactWebApp/static/js/main.169b0be1.chunk.js"
+  },
+  {
+    "revision": "c678ab02ed10843fc61d",
+    "url": "/MyFirstReactWebApp/static/js/runtime-main.c63aad7b.js"
+  }
+]);
