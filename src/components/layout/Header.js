d6f08c411cb4efc360b6e2caea8133a6cca99491
akhileshsingla <h20180260@pilani.bits-pilani.ac.in>
2020-05-08 17:44:04 +0530

    changed path in header.js for gh-pages

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -5,7 +5,7 @@ function Header() {
   return (
     <header style = {headerStyle}>
       <h1> ToDo List </h1>
-      <Link style={linkStyle} to="/">Home</Link> | <Link style={linkStyle} to="/about"> About </Link> 
+      <Link style={linkStyle} to="/MyFirstReactWebApp">Home</Link> | <Link style={linkStyle} to="/MyFirstReactWebApp/about"> About </Link> 
     </header>
   );
 };
